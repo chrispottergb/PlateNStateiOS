2934fe98e48ab765c2aff0e638612a4e0a94c3d2
chrispottergb <Chrispottergb@gmail.com>
2026-06-26 11:24:07 -0700

    Update build-ios.sh

--- build-ios.sh
+++ build-ios.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+set -e
+
+echo "[1/4] Installing dependencies..."
+npm install
+
+echo "[2/4] Building web project..."
+npm run build
+
+if [ ! -d "ios" ]; then
+  echo "[3/4] Adding iOS platform..."
+  npx cap add ios
+else
+  echo "[3/4] iOS platform already present, skipping add."
+fi
+
+echo "[4/4] Syncing web bundle into iOS project..."
+npx cap sync ios
 
 # Inject export compliance key
 PLIST="ios/App/App/Info.plist"
